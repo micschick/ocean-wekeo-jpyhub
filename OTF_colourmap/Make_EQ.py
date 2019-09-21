04b4e07050488541630e8eba4e50640c67fdfa04
Loveday <ben.loveday@external.eumetsat.int>
2019-09-21 15:11:49 +0000

    added log option to OTF colourmap

--- OTF_colourmap/Make_EQ.py
+++ OTF_colourmap/Make_EQ.py
@@ -6,13 +6,43 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import os
 
-def build_plot_command(vlimits,\
-                       red_widgets, green_widgets, blue_widgets):
+def process_data(VAR, LON, LAT, bbox, varname, reduce, log_var=False):
+    '''
+     Just some pre-processing. Probably requires extensive testing.
+    '''
+    if len(np.shape(VAR)) > 2:
+        VAR = np.squeeze(VAR[0,:,:])
+
+    if 'sst' in varname and np.nanmin(VAR) > 100:
+        VAR = VAR - 273.15
+
+    ii = np.where((LON >= bbox[0]) & (LON <= bbox[1]))[0]
+    jj = np.where((LAT >= bbox[2]) & (LAT <= bbox[3]))[0]
+
+    LON = LON[ii[0]:ii[-1]]
+    LAT = LAT[jj[0]:jj[-1]]
+    VAR = VAR[jj[0]:jj[-1],ii[0]:ii[-1]]
+
+    LON = LON[::reduce]
+    LAT = LAT[::reduce]
+    VAR = VAR[::reduce,::reduce]
+    
+    if log_var:
+        vlimits = [np.log10(np.nanmin(VAR)-0.01), np.log10(np.nanmax(VAR)+0.01), 255]
+        VAR = np.log10(VAR)
+    else:
+        vlimits = [int(np.nanmin(VAR))-1, int(np.nanmax(VAR))+1, 255]
+
+    return VAR, LON, LAT, vlimits
+
+def build_plot_command(vlimits, red_widgets, green_widgets, blue_widgets):
     
     runCMD = 'iplot = interactive(eq.plot_eq'\
+                        + ', varname=fixed(varname)' \
                         + ', var=fixed(VAR), lon=fixed(LON), lat=fixed(LAT)' \
                         + ', vmin=fixed(' + str(vlimits[0]) + ')' \
-                        + ', vmax=fixed(' + str(vlimits[1]) + '), ' \
+                        + ', vmax=fixed(' + str(vlimits[1]) + ')' \
+                        + ', log_var=fixed(log_var), ' \
                         + ', '.join(red_widgets) + ', ' \
                         + ', '.join(green_widgets) + ', ' \
                         + ', '.join(blue_widgets) + ')'
@@ -32,7 +62,7 @@ def make_widgets(channel_red, channel_green, channel_blue):
                             + str(channel_blue[ii]) + ', min=0.0, max=1.0, step=0.025)')
     return red_widgets, green_widgets, blue_widgets
 
-def plot_eq(var, lon, lat, vmin, vmax, **kwargs):
+def plot_eq(varname, var, lon, lat, vmin, vmax, log_var, **kwargs):
 
     nchannels = int(len(kwargs)/3)    
     RED_list = []
@@ -50,7 +80,7 @@ def plot_eq(var, lon, lat, vmin, vmax, **kwargs):
    
     x = np.linspace(vmin, vmax, nchannels)
 
-    plt.figure(figsize=(15,15))
+    plt.figure(figsize=(10,10))
     gs  = gridspec.GridSpec(2, 1, height_ratios=[1,4])
     
     RG_sensitivity = REDS + GREENS
@@ -64,10 +94,19 @@ def plot_eq(var, lon, lat, vmin, vmax, **kwargs):
     pn, = plt.plot(x, (BLUES+GREENS+REDS)/3, 'k--')
     pbr, = plt.plot(x, RG_sensitivity, '0.5', linestyle='--')
     plt.ylim(0, 1.1)
-    plt.xlabel('Channel')
-    plt.ylabel('intensity')
+    plt.xlabel('Channel (top: grey) & Mapped value (bottom: black)')
+    plt.ylabel('Channel intensity')
     leg1 = plt.legend([pr, pg, pb, pn, pbr],['red','green','blue','norm','r/g sens'])
-    plt.xticks(x)
+    
+    if log_var:
+        ticks = 10**(x)
+        newticks = []
+        for tick in ticks:
+            newticks.append(float(int(tick*100)/100.))
+        plt.xticks(x, newticks)
+    else:
+        plt.xticks(x)
+
     plt.xlim([vmin, vmax])
     plt.ylim([0, 1.25])
     for ii in range(nchannels):
@@ -96,8 +135,16 @@ def plot_eq(var, lon, lat, vmin, vmax, **kwargs):
     thismap = LinearSegmentedColormap('manual', cdict1)
 
     # plot variable
-    plt.pcolormesh(lon, lat, var, cmap=thismap, vmin=vmin, vmax=vmax)    
-    plt.colorbar()
+    plt.pcolormesh(lon, lat, var, cmap=thismap, vmin=vmin, vmax=vmax)
+    
+    if log_var:
+        cbar = plt.colorbar(ticks=x)
+        cbar.ax.set_yticklabels(newticks)
+    else:
+        cbar = plt.colorbar()
+      
+    cbar.set_label(varname, fontweight='bold', labelpad=10)
+
     plt.show()
     
     return ax1, ax2, REDS, GREENS, BLUES
